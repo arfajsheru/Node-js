34e7ae6b07a218c07754f33c04b09053c60ec4ba
arfajsheru <arfatsheru74@gmail.com>
2024-09-24 11:08:21 +0530

    add get methode worktype and stdtype

--- Practice2/db.js
+++ Practice2/db.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 const mongoDbUrl = process.env.MONGODB_URL;
 
 mongoose.connect(mongoDbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => {
     console.log('MongoDb connected')
 }).catch(() => {

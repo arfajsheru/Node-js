3c4445303b35e4bf97d9f44dfd19722f8cdf8b9c
arfajsheru <arfatsheru74@gmail.com>
2024-09-24 14:57:29 +0530

    add monogdb connection url

--- Practice2/db.js
+++ Practice2/db.js
@@ -3,16 +3,12 @@ require('dotenv').config();
 // const mongooseUrl = "mongodb://localhost:27017/hoteldb";
 // const mongoDbUrl = "mongodb+srv://arfajsheru:arfajsheru@cluster0.vsr22.mongodb.net/"
 // const mongoDbUrl = process.env.MONGODB_LOCAL_URL;
-const mongoDbUrl = process.env.MONGODB_URL;
 
-mongoose.connect(mongoDbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected');
-}).catch((error) => {
-    console.error('MongoDB connection error:', error.message);
-});
+mongoose.connect(process.env.MONGODB_URL, {
+  // Remove useNewUrlParser and useUnifiedTopology options
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('MongoDB connection error:', error));
 
 const db = mongoose.connection;
 
